0142bf96cf95064b2596e144f27e4fc182a09ce6
Mr-Proton <gavynpaulingee@gmail.com>
2022-09-11 17:10:12 +0530

    uploading-2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/users?_limit=8`)
+    fetch(`https://jsonplaceholder.typicode.com/users`)
       .then((response) => response.json())
       .then((actualData) => setData(actualData))
       .catch((err) => {
